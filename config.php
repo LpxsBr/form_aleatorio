6b31f1d6321e8483ab9429f4d6f9c5b0d3d71751
LpxsBr <anselmolopes.an@gmail.com>
2022-05-23 08:38:29 -0300

    atualização

--- config.php
+++ config.php
@@ -1,5 +1,36 @@
 <?php
-require_once('index.php');
-$nome_pessoa = $_POST["nome-da-pessoa"];
 
-echo $nome_pessoa;
+//testando o require do index.php
+// require_once('index.php');
+$nome_pessoa = $_POST["nome-da-pessoa"];
+$data_nascimento = $_POST["data-de-nascimento"];
+$facul_pessoa = $_POST["faculdade-da-pessoa"];
+$emprego_pessoa = $_POST["emprego-da-pessoa"];
+
+// $idade = $data_nascimento  - date();
+
+// var_dump($idade);
+
+//teste de "setagem"
+if($nome_pessoa and $data_nascimento and ($facul_pessoa or $emprego_pessoa)){
+try {
+    $conect = new PDO("mysql:dbname=formdb; host=localhost", "root", "");
+    echo '<br>'.'deu bom';
+} catch (PDOException $e) {
+    // throw $th
+    echo '<br>'.'deu ruim'.$e->getMessage();
+}
+// adicao dos dados
+$stmt = $conect->prepare("INSERT INTO form1(NOME, DATA_NASCIMENTO, EMPREGO, FACULDADE)
+VALUES(:a, :b, :c, :d)");
+try {
+    //code...
+    $stmt->bindparam(':a',$nome_pessoa);
+    $stmt->bindparam(':b',$data_nascimento);
+    $stmt->bindparam(':c',$emprego_pessoa);
+    $stmt->bindparam(':d',$facul_pessoa);
+    $stmt->execute();
+} catch (PDOException $e) {
+    //throw $th
+    echo "<br>".$e->getMessage();
+}}
--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <!DOCTYPE html>
+
+<!--
+    form
+    started in 21/05/2022.
+-->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="recursos/style.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="config.php" method="post">
-        <input type="text" class="campo-nome" name="nome-da-pessoa">
-    </form>
-    <?php
-    ?>
+    <div class="container">
+        <header class="header">
+        </header>
+        <section class="meio">
+            <form action="config.php" method="post">
+                nome<input type="text" class="campo-nome" name="nome-da-pessoa"><br>
+                data de nascimento<input type="date" class="campo-nome" name="data-de-nascimento"><br>
+                emprego<input type="text" class="campo-nome" name="emprego-da-pessoa"><br>
+                faculdade<input type="text" class="campo-nome" name="faculdade-da-pessoa"><br>
+                <button>Criar resumo</button>
+            </form>
+            <?php ?>
+        </section>
+        <footer class="rodape">
+        </footer>
+    </div>
 </body>
 </html>
