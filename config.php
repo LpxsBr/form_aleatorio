f9669158e805136828401fb1f4b0ff25e150c430
LpxsBr <anselmolopes.an@gmail.com>
2022-05-23 08:41:57 -0300

    atualização

--- config.php
+++ config.php
@@ -13,24 +13,28 @@ $emprego_pessoa = $_POST["emprego-da-pessoa"];
 
 //teste de "setagem"
 if($nome_pessoa and $data_nascimento and ($facul_pessoa or $emprego_pessoa)){
+
+    // conexão do banco de dados via PDO
 try {
     $conect = new PDO("mysql:dbname=formdb; host=localhost", "root", "");
     echo '<br>'.'deu bom';
 } catch (PDOException $e) {
-    // throw $th
     echo '<br>'.'deu ruim'.$e->getMessage();
 }
+
 // adicao dos dados
 $stmt = $conect->prepare("INSERT INTO form1(NOME, DATA_NASCIMENTO, EMPREGO, FACULDADE)
 VALUES(:a, :b, :c, :d)");
+
+// bind para passar os paramentros das variaveis para o bloco de sql
 try {
-    //code...
+
     $stmt->bindparam(':a',$nome_pessoa);
     $stmt->bindparam(':b',$data_nascimento);
     $stmt->bindparam(':c',$emprego_pessoa);
     $stmt->bindparam(':d',$facul_pessoa);
     $stmt->execute();
 } catch (PDOException $e) {
-    //throw $th
+
     echo "<br>".$e->getMessage();
 }}
