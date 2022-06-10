84060966df512a2ccbdfa1fbea56d513c2c2becb
Anselmo Lopes <96118395+LpxsBr@users.noreply.github.com>
2022-06-10 15:05:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,25 +9,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="recursos/style.css">
+    <link rel="stylesheet" href="recursos\style.css">
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-        <header class="header">
-        </header>
-        <section class="meio">
+    <div class="paidomeio">
+        <div class="meio">
             <form action="config.php" method="post">
-                nome<input type="text" class="campo-nome" name="nome-da-pessoa"><br>
-                data de nascimento<input type="date" class="campo-nome" name="data-de-nascimento"><br>
-                emprego<input type="text" class="campo-nome" name="emprego-da-pessoa"><br>
-                faculdade<input type="text" class="campo-nome" name="faculdade-da-pessoa"><br>
+                <h5>nome</h5>
+                <input type="text" class="campo-nome" name="nome-da-pessoa"><br>
+                <h5>data de nascimento</h5>
+                <input type="date" class="campo-nome" name="data-de-nascimento"><br>
+                <h5>emprego</h5>
+                <input type="text" class="campo-nome" name="emprego-da-pessoa"><br>
+                <h5>faculdade</h5>
+                <input type="text" class="campo-nome" name="faculdade-da-pessoa"><br>
                 <button>Criar resumo</button>
-            </form>
-            <?php ?>
-        </section>
-        <footer class="rodape">
-        </footer>
-    </div>
+        </div>
+    </div>       
 </body>
 </html>
